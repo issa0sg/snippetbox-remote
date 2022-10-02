33499d8dc4edeb0fd07d08e957b0881b818f803a
issa0sg <issa.sagi.eng@gmail.com>
2022-10-02 17:58:26 +0600

    Разделение по папкам 2

---
+++ cmd/web/handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello snippet test"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "GET запрещен", 405)
+		// w.WriteHeader(405)
+		// w.Write([]byte("GET запрещен"))
+		return
+	}
+	w.Write([]byte("Создание новой заметки"))
+}
